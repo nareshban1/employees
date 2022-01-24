5e99840ce96e08fc2cd7261fe54a05e48fd99b03
Naresh Ban <np03a170060@heraldcollege.edu.np>
2022-01-24 11:31:29 +0545

    Fixed search Issue. Added error message when no employees found during search

--- src/routes/EmployeeList/EmployeeList.jsx
+++ src/routes/EmployeeList/EmployeeList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import EmployeeTable from "../../components/EmployeeTable/EmployeeTable";
+import ErrorMessages from "../../components/ErrorMessages/ErrorMessages";
 import Header from "../../components/Header/Header";
 import Heading from "../../components/Heading/Heading";
 import SearchBar from "../../components/SearchBar/SearchBar";
@@ -17,18 +18,27 @@ const EmployeeList = () => {
     backgroundColor: "white",
   };
 
+  const errorStyles = {
+    color: "red",
+    backgroundColor: "#FFE0E0",
+  };
+
   const handleSearch = (e) => {
     if (e.key === "Enter") {
       if (searchQuery.length > 0) {
-        let filteredData = employees?.filter((data) =>
+        let filteredData = employeeData?.filter((data) =>
           Object.keys(data).some(
-            (k) => data[k].toString().toLowerCase().indexOf(searchQuery) !== -1
+            (k) =>
+              data[k]
+                .toString()
+                .toLowerCase()
+                .indexOf(searchQuery.toLowerCase()) !== -1
           )
         );
+        console.log(filteredData);
         setEmployees(filteredData);
       } else {
         setEmployees(employeeData);
-        // alert("Search Query must have more than 3 characters");
       }
     }
   };
@@ -52,6 +62,12 @@ const EmployeeList = () => {
       </Header>
 
       <EmployeeTable employees={employees} />
+      {employees.length === 0 && (
+        <ErrorMessages
+          styles={errorStyles}
+          errorMessage={" Sorry no employees found"}
+        />
+      )}
     </div>
   );
 };
